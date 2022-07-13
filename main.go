facd7817522ac961f0667d5cc179682d762a21f6
root <root@MarkPC>
2022-07-13 20:10:30 +0800

    exit-push

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "Crawler.net/wall"
+import (
+	_ "embed"
+
+	"Crawler.net/server/global"
+	"Crawler.net/server/global/config"
+	"Crawler.net/server/router"
+	"Crawler.net/wall"
+	jsoniter "github.com/json-iterator/go"
+)
+
+//go:embed package.json
+var AppPackage []byte
 
 func main() {
+	jsoniter.Unmarshal(AppPackage, &config.AppInfo)
+
+	// 初始化系统参数
+	global.Start()
 	// 下载 原神 壁纸
 	wall.Genshin()
+
+	// 启动 http 监听服务
+	router.Start()
 }
